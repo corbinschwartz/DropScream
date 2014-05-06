bd65cd350f8ecb6c8ce431d728a9a9b11323e7b5
corbinschwartz <corbin@host-studentw-136-95.dhcp.stevens-tech.edu>
2014-05-06 14:53:26 -0400

    Less false positives
    Uses normalized acceleration vector instead of only accelerometer value
    in y direction. Less false positives.

--- src/com/corbin/dropscream/MainActivity.java
+++ src/com/corbin/dropscream/MainActivity.java
@@ -23,6 +23,9 @@ import android.widget.Toast;
 public class MainActivity extends ActionBarActivity implements SensorEventListener{
 	private SensorManager manager;
 	private long tTriggered;
+	private int count;
+	//private float[] rollingValues;
+	//private int keepScreaming;
     @Override
     
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +34,7 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         manager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        
+        initialize();
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new SensorFragment())
@@ -77,31 +80,62 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
         manager.unregisterListener(this);
     }
 	    
+    private void initialize() {
+    	count = 0;
+    	//rollingValues = new float[]{9.81f,9.81f,9.81f};
+    	//keepScreaming = 0;
+    	tTriggered = 0;
+    }
+    
 	private void checkDrop(SensorEvent event) {
 		float[] values = event.values;
-		long currTime = System.currentTimeMillis();
-		if (values[1] < .15) 
-		{
-			if (currTime - tTriggered < 2000) {
+		//rollingValues[count%rollingValues.length]=values[1];
+		long currTime;
+		float x = values[0];
+	    float y = values[1];
+	    float z = values[2];
+		count++;
+		float a = (float) java.lang.Math.sqrt(x * x + y * y + z * z);
+		/*for (int i=0;i<rollingValues.length;i++) {
+			if (rollingValues[i] > -0.1f) {
 				return;
 			}
-			tTriggered = currTime;
+		}*/
+		if (a >=19 ) {
+			currTime = System.currentTimeMillis();
+			if (currTime - tTriggered < 1000) {
+				/*if (keepScreaming == 0) {
+					rollingValues[count%rollingValues.length] = 9.81f;
+				}*/
+				return;
+			}
 			scream();
-			Toast.makeText(this, "Ahh!", Toast.LENGTH_SHORT).show();
+			tTriggered = currTime;
+			Toast.makeText(this, Integer.toString(count) + ":" + Float.toString(a), Toast.LENGTH_SHORT).show();
+			count=0;
+			/*if (keepScreaming == 0) {
+				for (int i=0; i<rollingValues.length; i++) {
+					rollingValues[i] = 9.81f;
+				}
+			}*/
 		}
+		
 	}
 	
 	private void scream() {
-	    MediaPlayer mp = MediaPlayer.create(this, R.raw.scream);
-	    mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
-	    try {
-			mp.prepare();
-		} catch (IllegalStateException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+		long currTime = System.currentTimeMillis();
+		if (currTime - tTriggered > 1000) {
+			MediaPlayer mp = MediaPlayer.create(this, R.raw.scream);
+		    mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
+		    try {
+				mp.prepare();
+			} catch (IllegalStateException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		    mp.start();
 		}
-	    mp.start();
 	}
 
     public static class SensorFragment extends Fragment {
